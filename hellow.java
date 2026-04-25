49f4d3bada264c783ce2d5b981eb6b1aa4e8a70e
Shiva Urolagin <shiv.urolagin33@gmail.com>
2026-04-26 03:10:19 +0530

    Update hellow.java

--- hellow.java
+++ hellow.java
@@ -2,6 +2,6 @@ public class UserInfo {
     public static void main(String[] args) {
         System.out.println("Name: Shiva Urolagin");
         System.out.println("Email: Shiv@gmail.com");
-        System.out.println("Position: DevOps Engineer");
+        System.out.println("Position: DevOps Engineer3");
     }
 }
