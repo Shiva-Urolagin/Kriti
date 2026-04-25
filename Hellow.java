5441025a606fa69c52879479350a8dbc27cc4695
Shiva Urolagin <shiv.urolagin33@gmail.com>
2026-04-26 01:25:56 +0530

    Delete Hellow.java

--- Hellow.java
+++
@@ -1,7 +0,0 @@
-public class UserInfo {
-    public static void main(String[] args) {
-        System.out.println("Name: Shiva Urolagin");
-        System.out.println("Email: Shiv@gmail.com");
-        System.out.println("Position: DevOps Engineer");
-    }
-}
