586ef83f9c5190ff78a0d58023e6b5feec3d076b
Shiva Urolagin <shiv.urolagin33@gmail.com>
2026-04-26 00:22:34 +0530

    Update Hellow.java

--- Hellow.java
+++ Hellow.java
@@ -1,2 +1,7 @@
-HelloWorld
-
+public class UserInfo {
+    public static void main(String[] args) {
+        System.out.println("Name: Shiva Urolagin");
+        System.out.println("Email: Shiv@gmail.com");
+        System.out.println("Position: DevOps Engineer");
+    }
+}
